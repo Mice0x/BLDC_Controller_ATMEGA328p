557ef80e0432b1a93bbb98ca0b43515aa1441ee4
Mice0x <59442734+Mice0x@users.noreply.github.com>
2021-02-22 08:32:25 +0100

    Update Communication_Master.ino

--- Communication_Master/Communication_Master.ino
+++ Communication_Master/Communication_Master.ino
@@ -1,13 +1,13 @@
 #include <Wire.h>
-byte pwm = 255;
-bool dir = false;
-byte data [2] = {pwm, dir};
+byte pwm = 255; //PWM-Frequenz
+bool dir = false; //Richtung
+byte data [2] = {pwm, dir}; //data Array
 void setup() {
 Wire.begin();
 }
 
 void loop() {
-  Wire.beginTransmission(0x08);
-  Wire.write(data, 2);
-  Wire.endTransmission();
+  Wire.beginTransmission(0x08); //Beginne Kommunikation
+  Wire.write(data, 2); //Daten werden in dne Sendepuffer geschriebn
+  Wire.endTransmission(); //Daten werden übertragen
 }
